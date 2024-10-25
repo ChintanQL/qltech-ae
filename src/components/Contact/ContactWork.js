9b4b87b83e4d27639702f39e98baaeec6304e26b
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-10-25 10:49:44 +0530

    Update ContactWork.js

--- src/components/Contact/ContactWork.js
+++ src/components/Contact/ContactWork.js
@@ -173,7 +173,7 @@ class ContactWork extends React.Component{
 										<div className="col-md-12 mt-3">
 											<ReCAPTCHA
 												ref={recaptchaRef}
-												sitekey="6LfliiwqAAAAACFOqWuUVddqUrHE2DQUbaxTRNKk"
+												sitekey="6LdYnWsqAAAAABGf_YFPGhnRH_ltZ3el9VrbJX9D"
 											/>
 										</div>
 										
