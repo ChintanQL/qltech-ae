949860627f01b379f3e2d74f628303d1be8a4753
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-10-25 10:49:57 +0530

    Update ContactWorksheet.js

--- src/components/Contact/ContactWorksheet.js
+++ src/components/Contact/ContactWorksheet.js
@@ -188,7 +188,7 @@ class ContactWorksheet extends React.Component{
 										<div className="col-md-12 mt-3">
 											<ReCAPTCHA
 												ref={recaptchaRef}
-												sitekey="6LfliiwqAAAAACFOqWuUVddqUrHE2DQUbaxTRNKk"
+												sitekey="6LdYnWsqAAAAABGf_YFPGhnRH_ltZ3el9VrbJX9D"
 											/>
 										</div>
 										
