90249cc3217f025ba03e85bb061f52ee41c70d3f
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-10-25 10:50:12 +0530

    Update Webcontact.js

--- src/components/Contact/Webcontact.js
+++ src/components/Contact/Webcontact.js
@@ -139,7 +139,7 @@ const payload = {
 										<div className="col-md-12 mt-3">
 											<ReCAPTCHA
 												ref={recaptchaRef}
-												sitekey="6LfliiwqAAAAACFOqWuUVddqUrHE2DQUbaxTRNKk"
+												sitekey="6LdYnWsqAAAAABGf_YFPGhnRH_ltZ3el9VrbJX9D"
 											/>
 										</div>
 										
