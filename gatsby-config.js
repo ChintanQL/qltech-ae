77c2462e47113805d53600860520c996b20991c4
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-10-28 13:02:17 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,6 +83,21 @@ module.exports = {
       output: `/sitemap.xml`,
     }
   },
+	{
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "GTM-NKJ4XPD9",
+
+      // Include GTM in development.
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+    },
+  },  
   {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
