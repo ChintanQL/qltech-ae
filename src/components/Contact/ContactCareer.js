63cd038ce66e262386f09fb7c61f9e7d338657f3
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-10-28 10:11:19 +0530

    Update ContactCareer.js

--- src/components/Contact/ContactCareer.js
+++ src/components/Contact/ContactCareer.js
@@ -166,14 +166,14 @@ identify(identifyObj);
 console.log(payload);
 			axios({
 			  method: 'post',
-			  url: 'https://steamlinedesign.com/qltech/formbucket/',
+			  url: 'https://steamlinedesign.com/qltech/formbucket/uae.php',
 			  data: payload, // you are sending body instead
 			  headers: {
 			   // 'Authorization': `bearer ${token}`,
 			  'Content-Type': 'multipart/form-data'
 			  }, 
 			}).then(function(response) {
-			window.location.href = "https://www.qltech.com.au/thank-you/";
+			window.location.href = "https://www.qltech.ae/thank-you/";
 		    });
 		//document.getElementById('form').submit()
 	  } else {
