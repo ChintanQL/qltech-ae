817336514eafc1f4d14d0a90b196a2335ea0ecdb
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-11-04 09:25:24 +0530

    Update schedule-a-call.js

--- src/pages/schedule-a-call.js
+++ src/pages/schedule-a-call.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 import Layout from '../components/Layout'
-
+import Helmet from 'react-helmet'
 class Calendly extends React.Component {
   
   constructor(props) {
@@ -51,7 +51,12 @@ class Calendly extends React.Component {
   render(){
     return (
       <Layout>
-        <div id="schedule_form">
+	    <Helmet>
+		<title>Schedule a CALL  | Web development & marketing agency UAE, Abu Dhabi, Dubai- QL Tech</title>
+		<meta name="title" content="Schedule a CALL  | Web development & marketing agency UAE, Abu Dhabi, Dubai- QL Tech"></meta>
+		<meta name="description" content="QL Tech is a Leading Digital Marketing Agency. Our Services Include Web Development, Website Design, CRM Services, Social Media Marketing & SEO. Get Assured Leads & Sales Ensuring amplified ROI For Your Business Through PPC, Content Marketing & More In Dubai, Abu Dhabi, UAE."></meta>
+       </Helmet>
+	    <div id="schedule_form">
           <div 
             className="calendly-inline-widget"
             data-url={this.state.url}
